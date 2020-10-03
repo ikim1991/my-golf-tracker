d6c4dbb89c0e35d2dcd3f369db39a20088968f6e
Chris K <ikim1991@gmail.com>
2020-10-03 18:20:35 -0400

    Updates

---
+++ precache-manifest.c01914db3eb72df34e8164774be7c3c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da370db81a06a233b054fe6d063068c0",
+    "url": "/my-golf-tracker/index.html"
+  },
+  {
+    "revision": "39f5b4ebf3305544f460",
+    "url": "/my-golf-tracker/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "97f5c30ca2ea97425125",
+    "url": "/my-golf-tracker/static/css/main.96c324ae.chunk.css"
+  },
+  {
+    "revision": "39f5b4ebf3305544f460",
+    "url": "/my-golf-tracker/static/js/2.b9a71d4b.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/my-golf-tracker/static/js/2.b9a71d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f5c30ca2ea97425125",
+    "url": "/my-golf-tracker/static/js/main.eebef384.chunk.js"
+  },
+  {
+    "revision": "48be8f33d3de0ef5f264",
+    "url": "/my-golf-tracker/static/js/runtime-main.45246bd3.js"
+  }
+]);
